f035b137308ceacc52b95ac8aed51b9d417359d4
ZakyMalika <zakymalika0987@gmail.com>
2024-02-27 18:50:16 +0700

    Add project files.

---
+++ PraktikumPertamaPemrograman/PraktikumPertamaPemrograman.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//Begin
+    //numeric nJejari, nLuas
+    float nJejari, nLuas;
+    //display 'masukan jejari= '
+    cout << "masukan jejari =";
+    //accept nJejari
+    cin >> nJejari;
+    //compute nLuas = 3.14 * r * r
+    nLuas = 3.14 * nJejari * nJejari;
+    //display 'Luasnya= ' + nLuas
+    cout << "Luasnya =" << nLuas;
+
+}//End
